9fb30125ac478c7d9b3b716bd27f72e58620a6f7
Antoine Boucher <antoine.bou13@gmail.com>
2026-08-27 19:00:47 -0400

    feat: add presentation mode to stream profiles

--- src/stream_profile.hpp
+++ src/stream_profile.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "config.hpp"
+#include "presentation_state.hpp"
 
 #include <array>
 #include <cstddef>
@@ -11,9 +12,10 @@ struct StreamProfilePreset {
     int height;
     int fps;
     int bitrate_kbps;
+    PresentationMode presentation_mode;
 };
 
-const std::array<StreamProfilePreset, 4> &stream_profile_presets();
+const std::array<StreamProfilePreset, 5> &stream_profile_presets();
 const StreamProfilePreset *find_stream_profile(const char *name);
 void apply_stream_profile(PCONFIGURATION config,
                           const StreamProfilePreset &profile);
