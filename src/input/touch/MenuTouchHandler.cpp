ebcc2b573e89bf94b28cf86d8fb7cd1760a37057
Antoine Boucher <antoine.bou13@gmail.com>
2026-08-27 19:03:58 -0400

    feat: expose performance page in stream menu

--- src/input/touch/MenuTouchHandler.cpp
+++ src/input/touch/MenuTouchHandler.cpp
@@ -19,24 +19,47 @@
 #include "../../system/dispatcher.hpp"
 #include "TouchHandler.hpp"
 #include <Limelight.h>
+#include <cstdio>
 #include <memory>
 #include <vector>
 
 static const int button_size_y = 60;
 static const int button_size_x = 160;
 
-// Map the Row and Column numbers of each button to their corresponding
-// Touch Type Setting
 static std::map<int, std::map<int, N3dsTouchType>> button_map{
     {0, {{0, N3dsTouchType::GAMEPAD}, {1, N3dsTouchType::MOUSEPAD}}},
     {1, {{0, N3dsTouchType::KEYBOARD}, {1, N3dsTouchType::ABSOLUTE_TOUCH}}},
     {2, {{0, N3dsTouchType::DS_TOUCH}, {1, N3dsTouchType::MAGNIFY_TOUCH}}},
-    {3, {{0, N3dsTouchType::DEBUG_TOUCH}, {1, N3dsTouchType::DISABLED}}},
+    {3, {{0, N3dsTouchType::PERFORMANCE_TOUCH},
+         {1, N3dsTouchType::DISABLED}}},
 };
 
-MenuTouchHandler::MenuTouchHandler() { aptSetHomeAllowed(true); }
+MenuTouchHandler::MenuTouchHandler() {
+    aptSetHomeAllowed(true);
+    redraw();
+}
 
-MenuTouchHandler::~MenuTouchHandler() { aptSetHomeAllowed(false); }
+MenuTouchHandler::~MenuTouchHandler() {
+    consoleSelect(&DebugTouchHandler::topScreen);
+    aptSetHomeAllowed(false);
+}
+
+void MenuTouchHandler::redraw() {
+    const u64 now = svcGetSystemTick();
+    if (last_redraw_ticks != 0 &&
+        now - last_redraw_ticks < (SYSCLOCK_ARM11 / 4)) {
+        return;
+    }
+    last_redraw_ticks = now;
+
+    consoleSelect(&DebugTouchHandler::bottomScreen);
+    consoleClear();
+    std::printf("Moonlight N3DS - Quick\n\n");
+    std::printf(" Gamepad           Mousepad\n\n");
+    std::printf(" Keyboard          Absolute touch\n\n");
+    std::printf(" Mirror/Stretch    Magnify\n\n");
+    std::printf(" Performance       Exit stream\n");
+}
 
 void MenuTouchHandler::_handle_touch_down(touchPosition touch) {
     _handle_touch_hold(touch);
@@ -48,24 +71,21 @@ void MenuTouchHandler::_handle_touch_up(touchPosition touch) {
 }
 
 void MenuTouchHandler::_handle_touch_hold(touchPosition touch) {
+    redraw();
+
     touch.py = touch.py < GSP_SCREEN_WIDTH ? touch.py : (touch.py - 1);
     touch.px = touch.px < GSP_SCREEN_HEIGHT_BOTTOM ? touch.px : (touch.px - 1);
     int round_y = touch.py / button_size_y;
     int round_x = touch.px / button_size_x;
 
+    if (round_y < 0 || round_y > 3 || round_x < 0 || round_x > 1) {
+        message = nullptr;
+        return;
+    }
+
     N3dsTouchType touch_type = button_map[round_y][round_x];
     switch (touch_type) {
-    case (N3dsTouchType::GAMEPAD):
-        message = std::make_shared<TouchStateChangedMsg>(touch_type);
-        break;
-    case (N3dsTouchType::MOUSEPAD):
-        message = std::make_shared<TouchStateChangedMsg>(touch_type);
-        break;
-    case (N3dsTouchType::KEYBOARD):
-        message = std::make_shared<TouchStateChangedMsg>(touch_type);
-        break;
     case (N3dsTouchType::DISABLED):
-        // Signal to exit the stream
         message = std::make_shared<GenericEventMsg>(MessageType::EXIT_STREAM);
         break;
     default:
