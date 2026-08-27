d82f8d209730bbe25a66ec3d60356995ce348c87
Antoine Boucher <antoine.bou13@gmail.com>
2026-08-27 19:16:17 -0400

    feat: include configured bitrate in telemetry

--- src/stream_telemetry_store.hpp
+++ src/stream_telemetry_store.hpp
@@ -3,8 +3,10 @@
 #include "stream_telemetry.hpp"
 
 #include <cstddef>
+#include <cstdint>
 
 void reset_global_stream_telemetry();
+void set_global_stream_bitrate(std::uint32_t bitrate_kbps);
 void push_global_stream_telemetry(const StreamTelemetrySample &sample);
 StreamTelemetrySummary global_stream_telemetry_summary();
 std::size_t copy_global_stream_telemetry(StreamTelemetrySample *destination,
